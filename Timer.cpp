9eb0d4119bace5bd9d41848f5259a20c19a8a35e
Luca De Pasquale <luca170105@gmail.com>
2025-11-18 18:05:18 +0100

    creazione della classe Timer e implementazione dei suoi metodi

---
+++ Timer.cpp
@@ -0,0 +1,79 @@
+//
+// Created by depa on 18.11.25.
+//
+
+#include <iostream>
+#include <chrono>
+#include "Timer.h"
+void Timer::setUpTimer(){
+    if(duration <= 0){
+        throw std::invalid_argument("Duration must be greater than zero");
+    }
+    remainingTime=duration;
+    start = std::chrono::steady_clock::now();
+    isrunning = true;
+
+}
+// Get next timer tick and calculate seconds
+void Timer::getTick() {
+    const auto now = std::chrono::steady_clock::now();
+    const auto diff = std::chrono::duration_cast<std::chrono::seconds>(now - last_checked).count();
+
+    if (remainingTime > 0) {
+        if (diff >= 1) {
+            remainingTime -= static_cast<int>(diff);
+            last_checked = now;
+            notify();
+        }
+    } else
+        isrunning = false;
+    notify();
+}
+void Timer::stopTimer(){
+    isrunning = false;
+}
+void Timer::resumeTimer(){
+    isrunning = true;
+    getTick();
+    notify();
+
+}
+void Timer::restartTimer(){
+    if(isrunning){
+        throw std::invalid_argument("Timer is already running, stop the timer before restarting");
+    }
+    remainingTime = duration;
+    isrunning = false;
+    notify();
+}
+int Timer::getRemainingTime()const{
+    return remainingTime;
+}
+int Timer::getDuration()const{
+    return duration;
+}
+bool Timer::isRunning()const{
+    return isrunning;
+}
+void Timer::setDuration(int s){
+    if(s >=0){
+        duration = s;
+        remainingTime =duration;
+
+        notify();
+    }
+    else{
+        throw std::invalid_argument("one of the value is invalid or all of them");}
+}
+//observer Methods
+void Timer::subscribe(Observer* o){
+    observers.push_back(o);
+}
+void Timer::unsubscribe(Observer* o){
+    observers.remove(o);
+}
+void Timer::notify(){
+    for(const auto& o : observers){
+        o->update();
+    }
+}
---
+++ Timer.h
@@ -0,0 +1,51 @@
+//
+// Created by depa on 18.11.25.
+//
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "Subject.h"
+#include "Observer.h"
+#include <list>
+#include <vector>
+#include <chrono>
+
+class Timer :public Subject {
+    Timer (): remainingTime(0),isrunning(false),duration(0) {}
+    ~Timer() override{}
+    int getRemainingTime() const;
+    int getDuration() const;
+    bool isRunning() const;
+    void setDuration(int s);
+
+    void setUpTimer();
+    void stopTimer();
+
+    void restartTimer();
+    void resumeTimer();
+    void getTick();
+
+    void subscribe(Observer* o) override;
+    void unsubscribe(Observer* o) override;
+    void notify() override;
+
+
+
+
+private:
+    int remainingTime;
+    bool isrunning;
+    int duration;
+    std::list<Observer*> observers;
+
+    std::chrono::time_point<std::chrono::steady_clock> start;
+    std::chrono::time_point<std::chrono::steady_clock> last_checked;
+
+};
+
+
+
+
+
+#endif //TIMER_H
