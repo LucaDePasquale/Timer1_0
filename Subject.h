7c93854ef2220de490532a2da7900919b80cc63a
Luca De Pasquale <luca170105@gmail.com>
2025-11-18 17:58:32 +0100

    creazione della classe subject

---
+++ Subject.h
@@ -0,0 +1,16 @@
+//
+// Created by depa on 18.11.25.
+//
+
+#ifndef SUBJECT_H
+#define SUBJECT_H
+#include "Observer.h"
+class Subject {
+public:
+    virtual ~Subject() {}
+    virtual void subscribe(Observer* o) = 0;
+    virtual void unsubscribe(Observer* o) = 0;
+    virtual void notify() = 0;
+};
+
+#endif //SUBJECT_H
